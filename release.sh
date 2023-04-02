5d41f2a13e83d52d7e12fd12981865df3069652b
HotrodMaster <hotrod.master@outlook.com>
2023-04-03 01:37:15 +0300

    Update to lunar

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ dpkg-sig --sign builder ./output/pika-baseos*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-baseos*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-baseos*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-baseos*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-baseos*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
