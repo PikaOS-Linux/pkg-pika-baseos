9a270fb0dde39ac4e19c93f5317188730c93c1be
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-03-26 19:58:47 +0300

    Update 00-append-pika-ld.sh

--- pika-baseos/etc/profile.d/00-append-pika-ld.sh
+++ pika-baseos/etc/profile.d/00-append-pika-ld.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(echo $(for i in /usr/share/pika/ld.so.d/*; do cat $i && echo -e "\n" ; done) | grep -v '^#' | tr '\n' ':' | tr ' ' ':')"
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(echo $(for i in /usr/share/pika/ld.so.d/*; do cat $i && echo -e "\n" ; done) | grep -v '^#' | tr '\n' ':' | tr ' ' ':')"
