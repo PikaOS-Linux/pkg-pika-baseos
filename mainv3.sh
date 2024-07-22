a1f741d14cf217fa738ab09c139f7fba3fae9cc0
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-23 01:33:18 +0300

    Update to PikaOS 4

---
+++ mainv3.sh
@@ -0,0 +1,23 @@
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
+
+# Clone Upstream
+cd ./pika-baseos
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dh_make --createorig
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
--- pika-baseos/etc/profile.d/00-append-pika-ld.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(echo $(for i in /usr/share/pika/ld.so.d/*; do cat $i && echo -e "\n" ; done) | grep -v '^#' | tr '\n' ':' | tr ' ' ':')"
