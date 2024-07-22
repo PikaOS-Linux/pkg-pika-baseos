691e489439d822e24d3990009ca62203321bc667
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-23 01:37:26 +0300

    fix main and release

--- mainv3.sh
+++ mainv3.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+set -e
+
 export DEBIAN_FRONTEND="noninteractive"
 export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
 export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
@@ -14,8 +18,8 @@ cd ./pika-baseos
 apt-get build-dep ./ -y
 
 # Build package
-dh_make --createorig
-dpkg-buildpackage
+LOGNAME=root dh_make --createorig -y -l -p pika-baseos_3.0.0 || echo "dh-make didn't go clean"
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
